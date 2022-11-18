f72ebde6ab798ce6a62731faa302a3dee84eb541
Nuvadusmart <martin.haaland@yahoo.com>
2022-11-18 10:51:28 +0100

    Add files via upload
    try to make a github.io

---
+++ bird.js
@@ -0,0 +1,83 @@
+class Bird{
+    constructor(imgsrc, maxFramesX = 1,maxFramesY = 1,scale = 1){
+        this.x = 250;
+        this.y = 200;
+        this.vy = 0;
+        
+        this.weight = 0.1  ;
+        this.maxFramesX = maxFramesX;
+        this.maxFramesY = maxFramesY;
+        this.framesCurrentX = 0
+        this.framesCurrentY = 0
+        this.framesElapsed = 0
+        this.framesHold = 5
+        this.image = new Image();
+        this.image.src = imgsrc
+        this.scale = scale
+        this.width = this.image.width * this.scale//20;
+        this.height = this.image.height * this.scale//20;
+        this.offsetx = 15
+        this.offsety = 20
+
+    }
+    update(){
+        bird.draw2();
+        this.framesElapsed++
+        if(this.framesElapsed % this.framesHold === 0){
+    
+        
+        if(this.framesCurrentX < this.maxFramesX-1)
+            {this.framesCurrentX++ }
+            else{ this.framesCurrentX = 0;
+                    
+            }
+        if(this.framesCurrentY < this.maxFramesY && this.framesCurrentX == 0)
+            {this.framesCurrentY++ }
+            else {this.framesCurrentY = 0
+            }
+        }
+        
+
+        let curve = Math.sin(angle)*20;
+        if (this.y > canvas.height - (this.height*2) + curve){
+            this.y = canvas.height- (this.height*2) + curve;
+            this.vy = 0;
+
+        }else {
+
+        this.vy += this.weight;
+        this.y += this.vy;
+        }
+        if(this.y < 0 + this.height){
+            this.y = 0 + this.height;
+            this.vy =0;
+        }
+        if (spacePressed && this.y > this.height * 2 ) this.flap();
+    
+    }
+    draw(){
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.x,this.y,this.width,this.height);
+
+    }
+    draw2(){
+        ctx.drawImage(
+            this.image,                                                         //image
+            this.framesCurrentX * (this.image.width /this.maxFramesX),          //sx
+            this.framesCurrentY * (this.image.height / this.maxFramesY),        //sy
+            //0,                                                                 //sy
+            this.image.width / this.maxFramesX,                                  //swidth
+            this.image.height / this.maxFramesY,                                 //sheight
+            (this.x - this.offsetx),                                                             //dx - x.position
+            (this.y - this.offsety),                                                             //dy - y.position
+            (this.image.width / this.maxFramesX) * this.scale,                   //dwidth
+            (this.image.height / this.maxFramesY) * this.scale                 //dheight
+            )
+            
+    }
+    flap(){
+        this.vy -=.6;
+
+    }
+}
+const bird = new Bird('bird5x4.png',5,3,0.75);
---
+++ main.js
@@ -0,0 +1,93 @@
+const canvas = document.getElementById('canvas1');
+ctx = canvas.getContext('2d')
+const DEBUG = document.getElementById('debug');
+canvas.width = document.documentElement.clientWidth-100
+canvas.height = document.documentElement.clientHeight -100
+
+let spacePressed = false;
+let enterPressed = false;
+let angle = 0;
+let hue = 0;
+let frame = 0;
+let framesElapsed = 0;
+
+let score = 0;
+let gamespeed = 8;
+
+
+
+function animate(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    ctx.fillStyle = 'black'
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    //ctx.fillRect(10,temp,50,50);
+    bird.update();
+    handleCollisions();
+    
+    
+    
+    handleParticles();
+    handleObstacles();
+    showScore();
+    if(handleCollisions()) return; //jump out of animloop;
+    requestAnimationFrame(animate);
+    angle += 0.12; //wobblespeed;
+    hue +=1;
+    frame++;
+    let debugoutput = 'frame:'+frame+'<br>'+
+                      'currentframeX:'+bird.framesCurrentX+'<br>'+
+                      'currentframeY:'+bird.framesCurrentY+'<br>'  
+    DEBUG.innerHTML = debugoutput;
+   
+}
+
+animate();
+
+
+window.addEventListener('keydown',function(e){
+    console.log(e.code);
+    if(e.code ==='Space') spacePressed = true;
+    if(e.code ==='Enter') this.location.reload()
+    
+
+})
+
+window.addEventListener('keyup',function(e){
+    //console.log(e.code);
+    if(e.code ==='Space') spacePressed = false;
+    if(e.code ==='Enter') enterPressed = false;
+
+
+
+})
+
+const bang = new Image();
+bang.src = 'https://www.freepnglogos.com/uploads/explosion-png/huge-fireball-explosion-png-transparent-7.png'
+//'https://w7.pngwing.com/pngs/777/977/png-transparent-explode-game-orange-explosion-thumbnail.png'
+
+function handleCollisions(){
+    for(let i = 0; i < obstaclesArray.length; i++){
+        if (bird.x < obstaclesArray[i].x + obstaclesArray[i].width &&
+            bird.x + bird.width > obstaclesArray[i].x &&
+            ((bird.y < 0 + obstaclesArray[i].top && bird.y +bird.height > 0) ||
+            (bird.y > canvas.height - obstaclesArray[i].bottom
+                 && bird.y + bird.height < canvas.height))
+            ){
+                // collision detected
+                ctx.drawImage(bang,bird.x-(bird.offsetx),(bird.y-bird.offsety),50,50);
+                ctx.font = "25px Georgia";
+                ctx.fillStyle = 'white';
+                ctx.fillText('Game Over, your score is ' + score, 160, canvas.height/2 - 12.5)
+                return true;
+            }
+    }
+}
+
+function showScore(){
+    
+    ctx.font = "90px Georgia";
+    ctx.fillStyle = 'white';
+    ctx.strokeText(score, canvas.width - 200,100)
+    ctx.fillText(score, canvas.width - 200,100)
+
+}
